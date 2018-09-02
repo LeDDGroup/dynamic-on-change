93095a88a0d3824e0fa880a0de744c8614a7a982
Daniel Perez Alvarez <danielpza@protonmail.com>
2018-09-01 20:21:22 -0400

    Setup

--- test/index.ts
+++ test/index.ts
@@ -1,39 +1,41 @@
 import { Data, DataArray } from "../src/";
 
-interface IPerson {
-  name: string;
-  dob?: string;
-  age?: number;
-}
-const personData = new Data<IPerson>({
-  onChange: (value) => console.log(value),
-  defaultValue: {
-    name: "",
-    age: 18,
-  },
-});
-
-personData.onChange.name("some person name");
-personData.onChange.age(70);
-personData.onChange.dob("dob date");
-
-const personDataArray = new DataArray<IPerson>({
-  onChange: (value) => console.log(value),
-  defaultValue: [
-    {
+it("should work", () => {
+  interface IPerson {
+    name: string;
+    dob?: string;
+    age?: number;
+  }
+  const personData = new Data<IPerson>({
+    onChange: (value) => console.log(value),
+    defaultValue: {
       name: "",
       age: 18,
     },
-  ],
-});
+  });
 
-personData.data = { name: "erased" };
+  personData.onChange.name("some person name");
+  personData.onChange.age(70);
+  personData.onChange.dob("dob date");
 
-personDataArray.onChange[personDataArray.data.length]({
-  name: `data${personDataArray.data.length}`,
-});
-personDataArray.onChange[personDataArray.data.length]({
-  name: `data${personDataArray.data.length}`,
-});
+  const personDataArray = new DataArray<IPerson>({
+    onChange: (value) => console.log(value),
+    defaultValue: [
+      {
+        name: "",
+        age: 18,
+      },
+    ],
+  });
 
-personDataArray.data = [];
+  personData.data = { name: "erased" };
+
+  personDataArray.onChange[personDataArray.data.length]({
+    name: `data${personDataArray.data.length}`,
+  });
+  personDataArray.onChange[personDataArray.data.length]({
+    name: `data${personDataArray.data.length}`,
+  });
+
+  personDataArray.data = [];
+});
