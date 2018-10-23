59915b51d8e59712de35ac52704785ac3e46a26b
Daniel Perez Alvarez <danielpza@protonmail.com>
2018-10-23 15:37:47 -0400

    chore: add lint tools

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ export type dynamicOnChanges<T> = { [K in keyof T]: dynamicOnChange<T, K> };
 export function dynamicOnChange<T>(
   onChange: (key: keyof T, value: T[keyof T]) => void,
 ): dynamicOnChanges<T> {
-  return new Proxy({} as dynamicOnChanges<Required<T>>, {
-    get(target: dynamicOnChanges<Required<T>>, key: keyof T): dynamicOnChange<T, keyof T> {
-      if (target[key] === undefined) {
+  return new Proxy({} as dynamicOnChanges<T>, {
+    get(target: dynamicOnChanges<T>, key: keyof T): dynamicOnChange<T, keyof T> {
+      if ((target[key] as any) === undefined) {
         target[key] = (value: T[keyof T]): void => {
           onChange(key, value);
         };
